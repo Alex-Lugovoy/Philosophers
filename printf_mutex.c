0e962dd0edfa4a6ccbf467c798c6f3842e53e619
Brenett Sophia <bsophia@il-e1.msk.21-school.ru>
2021-07-02 16:59:50 +0300

    edit think

--- printf_mutex.c
+++ printf_mutex.c
@@ -26,9 +26,6 @@ void	printf_mutex_sleep(t_phil *philo)
 
 void	printf_mutex_think(t_phil *philo)
 {
-	pthread_mutex_lock(philo->output);
 	printf("%lu ms %d is thinking\n", ft_time(philo->st_time),
 		philo->philo_numb);
-	pthread_mutex_unlock(philo->output);
-	ft_wait(philo->ti_eat);
 }
